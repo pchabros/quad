9241a210b7d303cf1f9247b9ef8677f0f9d0160e
pchabros <pawel.chabros@yahoo.pl>
2025-02-02 20:24:07 +0100

    Create container returns container ID

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,6 +1,8 @@
 module Docker where
 
+import Data.Aeson ((.:))
 import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Types as Aeson.Types
 import qualified Network.HTTP.Simple as HTTP
 import RIO
 import qualified Socket
@@ -11,20 +13,43 @@ newtype Image = Image {name :: Text}
 newtype ContainerExitCode = ContainerExitCode Int
   deriving (Eq, Show)
 
+newtype ContainerId = ContainerId Text
+  deriving (Eq, Show)
+
 exitCodeToInt :: ContainerExitCode -> Int
 exitCodeToInt (ContainerExitCode code) = code
 
 newtype CreateContainerOptions = CreateContainerOptions {image :: Image}
 
-createContainer :: CreateContainerOptions -> IO ()
+parseResponse ::
+  HTTP.Response ByteString -> (Aeson.Value -> Aeson.Types.Parser a) -> IO a
+parseResponse res parser = do
+  let result = do
+        value <- Aeson.eitherDecodeStrict (HTTP.getResponseBody res)
+        Aeson.Types.parseEither parser value
+  case result of
+    Left e -> throwString e
+    Right status -> pure status
+
+createContainer :: CreateContainerOptions -> IO ContainerId
 createContainer options = do
   manager <- Socket.newManager "/var/run/docker.sock"
-  let body = Aeson.object [("Image", Aeson.toJSON options.image.name)]
+  let body =
+        Aeson.object
+          [ ("Image", Aeson.toJSON options.image.name)
+          , ("Tty", Aeson.toJSON True)
+          , ("Labels", Aeson.object [("quad", "")])
+          , ("Cmd", "echo hello")
+          , ("Entrypoint", Aeson.toJSON ["/bin/sh" :: String, "-c"])
+          ]
   let req =
         HTTP.defaultRequest
           & HTTP.setRequestManager manager
           & HTTP.setRequestMethod "POST"
           & HTTP.setRequestPath "/v1.47/containers/create"
           & HTTP.setRequestBodyJSON body
+  let parser = Aeson.withObject "create-container" $ \object -> do
+        cId <- object .: "Id"
+        pure $ ContainerId cId
   res <- HTTP.httpBS req
-  traceShowIO res
+  parseResponse res parser
